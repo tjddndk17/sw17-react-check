681114095944012fd47cc1a6cb08c7a895fe8dd6
전민경 <jmk@hunet.co.kr>
2021-11-15 09:53:08 +0900

    기업회원조회 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Login from './views/Login'
+import Login from "./views/Login";
+import CorpMember from "./views/member/CorpMember";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/login" element={<Login/>}  />
+      <Route exact path="/login" element={<Login />} />
+      <Route path="/member/corpMember" element={<CorpMember />} />
     </Routes>
   );
 }
